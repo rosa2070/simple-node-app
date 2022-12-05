d794cf4df8568e324536f1aca082de2acd3ceb52
jwpark12 <79896349+jwpark-sungshin@users.noreply.github.com>
2022-12-05 16:45:04 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 80;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world!!!\n');
+  res.send('Bye Bye Bye\n');
 });
 
 app.listen(PORT);
